cb0e3cf801b510ccee2ecd36e0511b2d3c5e6bf5
DI-TimDavenport <timdavenport@developersinstitute.co.nz>
2022-05-24 16:12:58 +1200

    Added basic functionality

--- src/Components/Collections.js
+++ src/Components/Collections.js
@@ -1,24 +1,63 @@
 import styled from "styled-components";
+
 import { Routes, Route, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import CollectionsGen0 from "./CollectionsGen0";
 
 const Collections = () => {
+  const [imgSrc, setImgSrc] = useState();
+  const [SSimgSrc, setSSImgSrc] = useState();
+
+  const BHImages = [
+    "https://imagedelivery.net/RT_RPMu352DnmQo9WAcrJw/d4aa389a-0789-42db-0042-8737ddb9bf00/previewimages",
+    "https://imagedelivery.net/RT_RPMu352DnmQo9WAcrJw/ff3ed493-7596-4a65-2d17-b42f2a924e00/previewimages",
+    "https://imagedelivery.net/RT_RPMu352DnmQo9WAcrJw/62d25cff-a13f-42bd-d75f-4a1a27b98800/previewimages",
+    "https://imagedelivery.net/RT_RPMu352DnmQo9WAcrJw/f7ec25b9-73fd-4106-c550-26cc1bc6ab00/previewimages",
+    "https://imagedelivery.net/RT_RPMu352DnmQo9WAcrJw/0f31b3bd-7971-4c85-40b9-824feb5b4200/previewimages",
+  ];
+
+  const SSImages = [
+    "https://imagedelivery.net/RT_RPMu352DnmQo9WAcrJw/d4aa389a-0789-42db-0042-8737ddb9bf00/previewimages",
+    "https://imagedelivery.net/RT_RPMu352DnmQo9WAcrJw/ff3ed493-7596-4a65-2d17-b42f2a924e00/previewimages",
+    "https://imagedelivery.net/RT_RPMu352DnmQo9WAcrJw/62d25cff-a13f-42bd-d75f-4a1a27b98800/previewimages",
+    "https://imagedelivery.net/RT_RPMu352DnmQo9WAcrJw/f7ec25b9-73fd-4106-c550-26cc1bc6ab00/previewimages",
+    "https://imagedelivery.net/RT_RPMu352DnmQo9WAcrJw/0f31b3bd-7971-4c85-40b9-824feb5b4200/previewimages",
+  ];
+
+  useEffect(() => {
+    setImgSrc(BHImages[Math.floor(Math.random() * 5)]);
+  }, []);
+
+  useEffect(() => {
+    setSSImgSrc(SSImages[Math.floor(Math.random() * 5)]);
+  }, []);
   return (
     <Wrapper className="container">
       <h1>BucketHead Studios Collections</h1>
       <CollList>
         <CollListItem className="ListItem">
           {" "}
-          <Link to="/collections/Gen0/standard">BH Gen 0</Link>
+          <Link to="/collections/Gen0/standard">
+            <Thumbnail
+              className="preview-thumbnail"
+              src={imgSrc}
+              alt="BH Preview Thumbnail"
+            />
+            <p>BH Gen 0</p>
+          </Link>
         </CollListItem>
         <CollListItem className="ListItem">
           {" "}
-          <Link to="/SolSabers">SolSabers</Link>
+          <Link to="/SolSabers"><Thumbnail
+            className="preview-thumbnail"
+            src={SSimgSrc}
+            alt="BH Preview Thumbnail"
+          /><p>SolSabers</p></Link>
         </CollListItem>
       </CollList>
-        <Routes>
-          <Route path="/Gen0/*" element={<CollectionsGen0 />} />
-        </Routes>
+      <Routes>
+        <Route path="/Gen0/*" element={<CollectionsGen0 />} />
+      </Routes>
     </Wrapper>
   );
 };
@@ -32,13 +71,13 @@ const Wrapper = styled.div`
 
 const CollList = styled.ul`
 display: grid;
-grid-template-rows: 1fr 1fr:
+grid-template-columns: 1fr:
 text-decoration: none;
 list-style:none;
 padding: 0;
 
 @media (min-width: 768px) {
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
     grid-gap: 1rem;
 }
 `;
@@ -56,4 +95,8 @@ const CollListItem = styled.li`
   :hover {
     background-color: yellow;
   }
+`;
+
+const Thumbnail = styled.img`
+  max-width: 100%;
 `;
